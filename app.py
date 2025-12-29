f8c34e4f3aed5333ec062037a20e6667cc70f11f
kawasemi0627-cpu <kawasemi0627@gmail.com>
2025-12-29 14:08:40 +0900

    fix:

--- app.py
+++ app.py
@@ -17,4 +17,7 @@ def index():
     return render_template("index.html", entries=entries)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
